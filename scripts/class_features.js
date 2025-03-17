13a19d5bf9ada522b00597b4ab42e1c929220a35
Lewis <lewisfulterer@gmail.com>
2025-03-17 19:36:19 +0000

    first commit

---
+++ scripts/class_features.js
@@ -0,0 +1,8 @@
+// Adds a new "Haida Infusion" class feature type
+Hooks.once("init", () => {
+  CONFIG.DND5E.featureTypes.class.subtypes.haidaInfusion = "Haida Infusion";
+});
+// Adds a new "Jujuhex" class feature type
+Hooks.once("init", () => {
+  CONFIG.DND5E.featureTypes.class.subtypes.jujuhex = "Jujuhex";
+});
